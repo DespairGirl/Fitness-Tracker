998cd9263cf15cdac9a3ddc3feed0cb78b8c4493
Ult Des <truttling.mariah@gmail.com>
2021-04-09 17:51:12 -0400

    fixing api routes

--- routes/api-route.js
+++ routes/api-route.js
@@ -22,7 +22,7 @@ router.get('/api/workouts', (req, res) => {
 
 });
 
-router.put("/api/workouts/:id", (req, res) => {
+router.put("/api/workouts/"+id, (req, res) => {
 
     db.Workout.findOneandUpdate(
         { _id: req.params.id },
